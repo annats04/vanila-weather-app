9444bf2c43758f3a472d4fca0b6022c4f128e81d
annats04 <82800599+annats04@users.noreply.github.com>
2021-05-03 20:53:32 +0200

    time update

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+let date = new Date(timestamp);
+let hours = date.getHours();
+ if (hours < 10) {
+    hours = `0${hours}`;
+  }
+let minutes = date.getMinutes();
+ if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+let days = ["Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"];
+let day = days[date.getDay()];
+    return `${day}  ${hours}:${minutes}`;
+}
+
+
 function displayTemperature(response) {
     console.log(response.data.main.temp);
     let temperatureElement= document.querySelector("#temperature");
@@ -11,6 +34,8 @@ console.log(response.data)
     humidityElement.innerHTML = response.data.main.humidity;
     let windElement = document.querySelector("#wind");
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 
